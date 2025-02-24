c92cfc469109393acae26694752155b6f176b268
Albert <albertpencas@gmail.com>
2025-02-24 16:35:50 +0100

    Aplicació catalog model de product

--- botiga/catalog/models.py
+++ botiga/catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)                       # Nombre del producto
+    description = models.TextField(blank=True)                    # Descripción opcional
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Precio
+    stock = models.IntegerField()                                 # Cantidad en stock
+    category = models.CharField(max_length=100)                   # Categoría del producto
+    image = models.CharField(max_length=255, blank=True)          # URL de la imagen (opcional)
+
+# def __str__(self):
+#     return self.name
