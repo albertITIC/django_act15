17db1cc56e57815ac0d9f6453e9dfd54dfcdb4af
Victor Fernandez <fernandezvictor738@gmail.com>
2025-02-28 16:48:28 +0100

    cambios modelos cart y orders

--- botiga/orders/models.py
+++ botiga/orders/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.db import models
 from catalog.models import Product  
-from cart.models import Cart 
-# Create your models here.
 
 class CustomUser(models.Model):
     name = models.CharField(max_length=100)
@@ -11,7 +8,7 @@ class CustomUser(models.Model):
 
 class Order(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    cart = models.OneToOneField(Cart, on_delete=models.CASCADE)  
+    cart = models.OneToOneField("cart.Cart", on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
