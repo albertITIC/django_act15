6ad38fc42b691299fc1bc2fc1ce290e653b3a987
Victor Fernandez <fernandezvictor738@gmail.com>
2025-03-03 23:56:00 +0100

    correciones

--- botiga/catalog/urls.py
+++ botiga/catalog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('products/', views.get_all_products, name='list_products'),  # Obtener todos los productos
     path('products/create/', views.create_product, name='create_product'),  # Crear un producto
-    path('products/<int:pk>/', views.get_product_by_id, name='get_product'),  # Obtener un producto
-    path('products/<int:pk>/update/', views.update_product, name='update_product'),  # Actualizar producto
-    path('products/<int:pk>/delete/', views.delete_product, name='delete_product'),  # Eliminar producto
+    path('products/<int:product_id>/', views.get_product_by_id, name='get_product'),  # Obtener un producto
+    path('products/<int:product_id>/update/', views.update_product, name='update_product'),  # Actualizar producto
+    path('products/<int:product_id>/delete/', views.delete_product, name='delete_product'),  # Eliminar producto
 ]
